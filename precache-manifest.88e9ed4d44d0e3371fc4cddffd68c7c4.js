58665de353ad84da0b076899329640c7b1aa181a
Nikita Bayev <nikita@bayev.kz>
2019-01-30 20:00:16 +0600

    Updates

---
+++ precache-manifest.88e9ed4d44d0e3371fc4cddffd68c7c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7e3fc8db03c2e47b105",
+    "url": "/streets-almaty/static/js/runtime~main.b7e3fc8d.js"
+  },
+  {
+    "revision": "7c11be032bb156d4aec0",
+    "url": "/streets-almaty/static/js/main.7c11be03.chunk.js"
+  },
+  {
+    "revision": "37cb807c1e6d930a2ab1",
+    "url": "/streets-almaty/static/js/1.37cb807c.chunk.js"
+  },
+  {
+    "revision": "7c11be032bb156d4aec0",
+    "url": "/streets-almaty/static/css/main.7c700db5.chunk.css"
+  },
+  {
+    "revision": "0e702727882d99581075275403482314",
+    "url": "/streets-almaty/index.html"
+  }
+];
